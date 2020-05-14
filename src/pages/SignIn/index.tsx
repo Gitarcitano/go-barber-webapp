1e210e8b0f836445149324a77d3c6ea07892b4e1
Giovanne Tarcitano <giovannetarcitano@gmail.com>
2020-05-14 01:59:52 -0300

    Finish login layout

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+        <>
+          <FiMail />
+          <input placeholder="E-mail" />
+        </>
+        <div>
+          <FiLock />
+          <input placeholder="Senha" />
+        </div>
+        <button type="submit">Entrar</button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+
+    <Background />
+  </Container>
+);
+
+export default SignIn;
